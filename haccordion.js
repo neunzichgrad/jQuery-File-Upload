c06e7718467a9be06e906f3e8caa4aa8a62c6be5
neunzichgrad <ronbayer89@live.de>
2013-01-02 03:12:42 -0800

    Create haccordion.js

---
+++ haccordion.js
@@ -0,0 +1,26 @@
+.haccordion{
+padding: 0;
+}
+
+.haccordion ul{
+margin: 0;
+padding: 0;
+list-style: none;
+overflow: hidden; /*leave as is*/
+}
+
+
+.haccordion li{
+margin: 0;
+padding: 0;
+display: block; /*leave as is*/
+width: 100%; /*For users with JS disabled: Width of each content*/
+height: 200px; /*For users with JS disabled: Height of each content*/
+overflow: hidden; /*leave as is*/
+float: left; /*leave as is*/
+}
+
+.haccordion li .hpanel{
+width: 100%; /*For users with JS disabled: Width of each content*/
+height: 200px;  /*For users with JS disabled: Height of each content*/
+}
